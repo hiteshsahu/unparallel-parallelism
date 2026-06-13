277c1c90472b22f7407624cdf83b31d00a2994d4
hiteshsahu <hiteshkrsahu@gmail.com>
2026-06-13 10:13:28 +0200

    Add error checks for cuda call

---
+++ src/cuda/cuda_check.cuh
@@ -0,0 +1,16 @@
+#pragma once
+#include <cstdlib>
+#include <iostream>
+
+/**
+ * Checks error in cuda calls
+ **/
+#define CUDA_CHECK(call)                                                        \
+    do {                                                                        \
+        cudaError_t err = (call);                                               \
+        if (err != cudaSuccess) {                                               \
+            std::cerr << "CUDA error at " << __FILE__ << ":" << __LINE__        \
+                      << " — " << cudaGetErrorString(err) << "\n";              \
+            std::exit(EXIT_FAILURE);                                            \
+        }                                                                       \
+    } while (0)
